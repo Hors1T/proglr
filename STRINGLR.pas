b1469be8cb8a1e1958a999a15a99271f12ceb94a
Hors1T <59093424+Hors1T@users.noreply.github.com>
2020-12-05 14:46:36 +0400

    Add files via upload

---
+++ STRINGLR.pas
@@ -0,0 +1,41 @@
+﻿uses
+  crt;
+var
+  text,slovo:string;
+  ch:char;
+  f:integer;
+  begin
+    f:=0;
+   ch:=readkey;
+    repeat
+      while ch<>#0 do begin
+        f:=1;
+        write(ch);
+        if ch<>' ' then 
+            slovo:=slovo+ch;  
+        if (ch = ' ') then 
+          begin
+            if (length(slovo) mod 2) = 0 then
+              begin
+                text:=text+slovo;
+                text:=text+' ';
+                slovo:='';
+              end;
+              slovo:='';
+         end;
+          ch:=readkey;
+      end;
+      ch:=readkey;
+      until ch=#112;
+       if (length(slovo) mod 2) = 0 then
+        begin
+          text:=text+slovo;
+          text:=text+' ';
+          slovo:='';
+        end;
+      writeln;
+      if(f=1)then
+      writeln(text)
+      else
+        writeln('Текста нет');
+  end.
